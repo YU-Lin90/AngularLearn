1603971c507abe65002a41d7ab70cd9dcd57c0ba
YuChenLin <andy50152@gmail.com>
2022-12-17 11:51:29 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  filter: 'all' | 'active' | 'done' = 'all';
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+  addItem(description: string) {
+    const newTodo = {
+      description,
+      done: false
+    }
+    this.allItems.unshift(newTodo);
+
+    localStorage.setItem('todo',JSON.stringify(newTodo))
+  }
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
 }
